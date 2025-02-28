662a58ada66ac8dc1f5907a630d5e1fe25943513
hiteshmourya21 <hiteshmourya221210@acropolis.in>
2025-02-28 17:02:36 +0530

    Completed Single User Model

---
+++ StuddyBuddy/studybuddy/src/pages/user/ProfilePage.js
@@ -0,0 +1,50 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import ProfileHeader from "../../components/User/ProfileHeader";
+import AboutSection from "../../components/User/AboutSection";
+import EducationSection from "../../components/User/EducationSection";
+import { axiosInstance } from "../../lib/axios.js";
+
+
+const ProfilePage = () => {
+    const {username} = useParams();
+    const queryClient = useQueryClient();
+
+    const {data:authUser, isLoading} = useQuery({queryKey:['authUser']});
+
+    const {data: userProfile, isLoading: isUserProfileLoading} = useQuery({
+        queryKey: ['userProfile', username],
+        queryFn:  () => axiosInstance.get(`/users/${username}`),
+    })
+
+    const {mutate:updateProfile} = useMutation({
+        mutationFn: async (updatedData) =>{
+            await axiosInstance.put("/users/profile",updatedData);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries(["userProfile", username]);
+            toast.success("Profile updated successfully");
+        }
+    })
+
+    if( isLoading || isUserProfileLoading) return null;
+
+    const isOwnProfile = authUser.username === userProfile.data.username;
+    const userData = isOwnProfile? authUser : userProfile.data;
+
+
+    const handleSave = (updatedData) => {
+        updateProfile(updatedData);
+    }
+
+  return (
+    <div className="max-w-4xl mx-auto p-4">
+        <ProfileHeader userData = {userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+        <AboutSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+	    <EducationSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+    </div>
+  )
+}
+
+export default ProfilePage
