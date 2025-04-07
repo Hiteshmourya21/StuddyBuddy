74a10d539a41a2afcca0472164ce65fa985811fe
kishori_garg <143869466+kishorigarg@users.noreply.github.com>
2025-04-08 00:30:14 +0530

    Delete StuddyBuddy directory

--- StuddyBuddy/frontend/src/api/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-// src/api/axiosConfig.js
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your backend URL
-  withCredentials: true, // Allow sending cookies if needed
-});
-
-export default instance;
